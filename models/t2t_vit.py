38298e056c5f01d0b01b4a41a1681cb9b41cebc5
YuanLi <33076543+yuanli2333@users.noreply.github.com>
2021-02-15 11:31:57 +0800

    Update t2t_vit.py

--- models/t2t_vit.py
+++ models/t2t_vit.py
@@ -1,3 +1,8 @@
+# Copyright (c) [2012]-[2021] Shanghai Yitu Technology Co., Ltd.
+#
+# This source code is licensed under the Clear BSD License
+# LICENSE file in the root directory of this file
+# All rights reserved.
 """
 T2T-ViT
 """
