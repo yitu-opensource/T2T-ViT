07b0eec084636b981174efccdc301d40286ac105
徐铭远 <43675899+xmy0916@users.noreply.github.com>
2022-01-17 22:24:53 +0800

    fix a bug
    fix a bug

--- utils.py
+++ utils.py
@@ -80,7 +80,7 @@ def load_state_dict(checkpoint_path, model, use_ema=False, num_classes=1000, del
 
 
 def load_for_transfer_learning(model, checkpoint_path, use_ema=False, strict=True, num_classes=1000):
-    state_dict = load_state_dict(checkpoint_path, use_ema, num_classes)
+    state_dict = load_state_dict(checkpoint_path, model, use_ema, num_classes)
     model.load_state_dict(state_dict, strict=strict)
 
 
